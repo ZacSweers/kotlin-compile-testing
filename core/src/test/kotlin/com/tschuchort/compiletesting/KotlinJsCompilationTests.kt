fc64a693db6c49cb14cb2aefbc7a824a1460fa86
Zac Sweers <pandanomic@gmail.com>
2023-05-28 20:59:35 -0400

    Disable KotlinJsCompilationTests for now

--- core/src/test/kotlin/com/tschuchort/compiletesting/KotlinJsCompilationTests.kt
+++ core/src/test/kotlin/com/tschuchort/compiletesting/KotlinJsCompilationTests.kt
@@ -7,11 +7,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
 import org.jetbrains.kotlin.compiler.plugin.CliOption
 import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import java.io.File
 
+@Ignore("These no longer work with Kotlin JS IR")
 @Suppress("MemberVisibilityCanBePrivate")
 class KotlinJsCompilationTests {
 	@Rule @JvmField val temporaryFolder = TemporaryFolder()
